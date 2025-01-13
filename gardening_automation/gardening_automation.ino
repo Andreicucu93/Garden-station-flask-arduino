336baba79d4c717663ef3224b5ca4783c87334a7
unknown <Andrei_Cucu@outlook.com>
2025-01-13 20:43:35 +0200

    The soil moisture sensor is integrated in this update

--- gardening_automation/gardening_automation.ino
+++ gardening_automation/gardening_automation.ino
@@ -3,19 +3,23 @@
 #define BLUE_PIN 7
 #define BUTTON_PIN 2
 #define RELAY_PIN A0
+#define SOIL_SENSOR_PIN A1
 
 int buttonStatus = HIGH; // Store the current button status
 int pinValue;            // Store the read value from the button
+unsigned long lastSoilReading = 0; // Last soil read
+const unsigned long soilInterval = 3000; // Read soil delay
 
 void setup() {
+    pinMode(SOIL_SENSOR_PIN, INPUT);
     pinMode(RELAY_PIN, OUTPUT);
     pinMode(RED_PIN, OUTPUT);
     pinMode(GREEN_PIN, OUTPUT);
     pinMode(BLUE_PIN, OUTPUT);
-    pinMode(BUTTON_PIN, INPUT_PULLUP);  // internal pull-up resistor
+    pinMode(BUTTON_PIN, INPUT_PULLUP);  // Use internal pull-up resistor
     Serial.begin(9600);
 
-    // Safely have everything turned off
+    // Turn off everything initially
     digitalWrite(RELAY_PIN, LOW);
     digitalWrite(RED_PIN, HIGH);
     digitalWrite(GREEN_PIN, LOW);
@@ -23,6 +27,13 @@ void setup() {
 }
 
 void loop() {
+    unsigned long currentMillis = millis();
+    if (currentMillis - lastSoilReading >= soilInterval) {
+        lastSoilReading = currentMillis; // Update time last read
+        int soilMoisture = analogRead(SOIL_SENSOR_PIN);
+        Serial.print("Soil moisture: ");
+        Serial.println(soilMoisture);
+    }
     pinValue = digitalRead(BUTTON_PIN);
     delay(10); // Debounce delay
 
