81e5e8d5914c03232d8fcdcb7c087506ec7a1444
unknown <Andrei_Cucu@outlook.com>
2025-01-25 18:54:44 +0200

    Implemented garden watering via soil moisture sensor

--- gardening_automation/gardening_automation.ino
+++ gardening_automation/gardening_automation.ino
@@ -5,18 +5,23 @@
 #define RELAY_PIN A0
 #define SOIL_SENSOR_PIN A1
 
-int buttonStatus = HIGH; // Store the current button status
-int pinValue;            // Store the read value from the button
-unsigned long lastSoilReading = 0; // Last soil read
-const unsigned long soilInterval = 3000; // Read soil delay
+int buttonStatus = HIGH; // Button status
+int pinValue;
+unsigned long lastSoilReading = 0;
+unsigned long lastPumpRun = 0;
+const unsigned long soilInterval = 1500; // Read soil delay
+const unsigned long pumpRunTime = 10000;  // Pump runs for 10 sec
+const unsigned long pumpCooldown = 15000; // Wait 15 sec before next pump // cooldown safety
+bool isPumping = false;
 
-void setup() {
+void setup()
+{
     pinMode(SOIL_SENSOR_PIN, INPUT);
     pinMode(RELAY_PIN, OUTPUT);
     pinMode(RED_PIN, OUTPUT);
     pinMode(GREEN_PIN, OUTPUT);
     pinMode(BLUE_PIN, OUTPUT);
-    pinMode(BUTTON_PIN, INPUT_PULLUP);  // Use internal pull-up resistor
+    pinMode(BUTTON_PIN, INPUT_PULLUP);
     Serial.begin(9600);
 
     // Turn off everything initially
@@ -26,30 +31,58 @@ void setup() {
     digitalWrite(BLUE_PIN, LOW);
 }
 
-void loop() {
+void loop()
+{
     unsigned long currentMillis = millis();
-    if (currentMillis - lastSoilReading >= soilInterval) {
-        lastSoilReading = currentMillis; // Update time last read
-        int soilMoisture = analogRead(SOIL_SENSOR_PIN);
+
+    // Read soil moisture per soilInterval
+    static int soilMoisture = 0;
+    if (currentMillis - lastSoilReading >= soilInterval)
+    {
+        lastSoilReading = currentMillis;
+        soilMoisture = analogRead(SOIL_SENSOR_PIN);
         Serial.print("Soil moisture: ");
         Serial.println(soilMoisture);
     }
-    pinValue = digitalRead(BUTTON_PIN);
-    delay(10); // Debounce delay
 
-    if (buttonStatus != pinValue) { // Button state has changed
+    // Button handling
+    pinValue = digitalRead(BUTTON_PIN);
+    delay(10); // Debounce
+
+    if (buttonStatus != pinValue)
+    {
         buttonStatus = pinValue;
 
-        if (buttonStatus == LOW) { // Button was pressed
+        if (buttonStatus == LOW)
+        {
             Serial.println("Button pressed!");
             digitalWrite(RED_PIN, LOW);
             digitalWrite(GREEN_PIN, HIGH);
             digitalWrite(RELAY_PIN, HIGH);
-        } else { // Button was released
+        }
+        else
+        {
             Serial.println("Button released!");
             digitalWrite(GREEN_PIN, LOW);
             digitalWrite(RED_PIN, HIGH);
             digitalWrite(RELAY_PIN, LOW);
         }
+    }
+
+    // Automatic watering logic
+    if (soilMoisture >= 600 && !isPumping && (currentMillis - lastPumpRun >= pumpCooldown))
+    {
+        Serial.println("Soil is dry! Turning on pump.");
+        digitalWrite(RELAY_PIN, HIGH);
+        lastPumpRun = currentMillis;
+        isPumping = true;
+    }
+
+    // Stop the pump after / per pumpRunTime
+    if (isPumping && (currentMillis - lastPumpRun >= pumpRunTime))
+    {
+        Serial.println("Turning off pump.");
+        digitalWrite(RELAY_PIN, LOW);
+        isPumping = false;
     }
 }
